2bf47102cc0bf0074f92fa50bdcb398d5fb23973
ThisIsntPermanent <102081493+ThisIsntPermanent@users.noreply.github.com>
2022-03-28 23:32:06 -0500

    Update README.m

--- WeekTwo/README.m
+++ WeekTwo/README.m
@@ -1,1 +1,6 @@
-Overview of Week Two
+Final Project for CS371 - Jiali Graham and Eric Xing 
+
+Week Two's Progress (3-28-22 to 4-1-22)
+  Currently we have five different files, the Driver_XOR, FC_Layer, Net, ReLU, and Activation Layer file.
+  
+  
