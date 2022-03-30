c01bbaff5ae9cc1aaa53e49d51cfcdc1316a92a2
ThisIsntPermanent <102081493+ThisIsntPermanent@users.noreply.github.com>
2022-03-30 14:25:52 -0500

    Update FC_Layer.m

--- WeekTwo/FC_Layer.m
+++ WeekTwo/FC_Layer.m
@@ -1,5 +1,7 @@
+% This file is the Fully-Connected layer which runs the data through and affects it with the weights and bias.
 classdef FC_Layer
 
+% Listing all the needed properties
     properties
         weight
         bias
@@ -7,19 +9,23 @@ classdef FC_Layer
         out
     end
 
+% Declaring method non-static
     methods(Static = false)
 
+% This function calculates the weight and bias used in the forward method
         function obj = FC_Layer(in, out) 
             obj.weight = rand(in, out) - 0.5;
             obj.bias = rand(1, out) - 0.5;
         end
 
+% This function moves the data forward and calculates it with the weight and bias before pushing it 'out' (returning the value)
         function [output, obj] = forward(obj, in)
             obj.in = in;
             obj.out = obj.in * obj.weight + obj.bias;
             output = obj.out;
         end
         
+        % This function is for backpropogation and readjusts the weight and bias, therefore reducing the loss
         function [dEdX, obj] = back(obj, loss, LR)
 
 %             disp(["OBJ WEIGHT" size(obj.weight)])
