d6af5604acd6aac8a072b6f76d42995d14e651cd
ThisIsntPermanent <102081493+ThisIsntPermanent@users.noreply.github.com>
2022-03-30 15:37:44 -0500

    Update Step_LR_Scheduler.m

--- WeekTwo/Step_LR_Scheduler.m
+++ WeekTwo/Step_LR_Scheduler.m
@@ -1,5 +1,7 @@
+% This is the Step Scheduler file which adjusts the learning rate for a smoother descent
 classdef Step_LR_Scheduler
-
+    
+    % This lists variables needed
     properties
         GAMMA
         STEP_SIZE
@@ -8,13 +10,15 @@ classdef Step_LR_Scheduler
     end
 
     methods
+        % This has all the original values inputted in
         function obj = Step_LR_Scheduler(STEP_SIZE, GAMMA, LR)
             obj.STEP_SIZE = STEP_SIZE;
             obj.GAMMA = GAMMA;
             obj.idx = 0;
             obj.LR = LR;
         end
-
+        
+        % After every step size (ie. if stepsize is 3 then after every 3 iterations) it decreases the learning rate 
         function LR = step(obj)
             obj.idx = obj.idx + 1;
             if mod(obj.idx, obj.STEP_SIZE) == obj.STEP_SIZE
