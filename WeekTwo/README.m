2160b1368950dcbbe6f1b26d8c910e1a49aaeac3
ThisIsntPermanent <102081493+ThisIsntPermanent@users.noreply.github.com>
2022-03-31 15:53:17 -0500

    Update README.m

--- WeekTwo/README.m
+++ WeekTwo/README.m
@@ -1,7 +1,7 @@
 Final Project for CS371 - Jiali Graham and Eric Xing 
 
 Week Two's Progress (3-28-22 to 4-1-22)
-  Currently we have five different files, the Driver_XOR, FC_Layer, Net, ReLU, and Activation Layer file. We also have the basic feed-forward mechanism (FC and Activation Layers) and back propogation (calculation of the Loss and editing weights through gradient loss).
+  Currently we have five different files, the Driver_XOR, FC_Layer, Net, ReLU, and Activation Layer file. We also have the basic feed-forward mechanism (FC and Activation Layers) and back propogation (calculation of the Loss and editing weights through gradient loss). In addition, we have been adding comments onto the code to make it more understandable for an outside perspective. 
   
   Driver_XOR: contains XOR test, expected outputs are [0, 1, 1, 0] and so far are sucessful.
   FC_Layer: creates a fully-connected layer and pushes the data to the next layer
@@ -14,6 +14,11 @@ Issues
     Solved: We took the derivative correctly.
 
 We have deviated from our project proposal slightly, by making the ANN more comprehensible compared to other programs before moving onto CNN. Therefore, future plans have altered somewhat. In Week Eleven we will be working to make our code accomodate CNN models.
+
+Eric's Contributions:
+
+Jiali's Contributions:
+Writing 
 
 
   
