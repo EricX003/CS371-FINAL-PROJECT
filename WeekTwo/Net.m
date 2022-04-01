eb924db38f8dd151c9537c07c1736d451db16266
EricX003 <90646214+EricX003@users.noreply.github.com>
2022-04-01 14:21:11 -0500

    Update Net.m

--- WeekTwo/Net.m
+++ WeekTwo/Net.m
@@ -1,4 +1,4 @@
-% The Net is where the model is trained and all the layers are added
+% Maintains cell array of layers, has capabilities to initialize, append layers, train, and predict
 classdef Net
     
     % List of needed variables
@@ -22,14 +22,14 @@ classdef Net
             obj.layers{end + 1} = layer;
         end
         
-        % Outputs prediction which causes the network to adjust its weights based off of this prediction to achieve more accuracy
+        % Outputs prediction on each datum
         function prediction = predict(obj, x)
             prediction = {};
             for data_idx = 1:length(x)
 
                     data = x{data_idx};
                     
-                    % Bases prediction off of the last layers results
+                    % Prediction is value after passed though all layers
                     for idx = 1:length(obj.layers)
                         [data, obj.layers{idx}] = obj.layers{idx}.forward(data);
                     end
@@ -42,7 +42,7 @@ classdef Net
         % Function is for training the model 
         function model = train(obj, x, y, EPOCHS, LR)
             
-            % Calls upong the Step_LR_Scheduler to adjust the learning rate
+            % Initialize Step_LR_Scheduler to adjust the learning rate
             scheduler = Step_LR_Scheduler(1000, 0.3, LR);
 
             last = -1;
@@ -55,21 +55,21 @@ classdef Net
 
                     data = x{data_idx};
                     
-                    % Stores the data
+                    % Find prediction
                     for idx = 1:length(obj.layers)
                         [data, obj.layers{idx}] = obj.layers{idx}.forward(data);
                     end
                     
-                    % Keeps track of the total ammount of loss 
+                    % Keeps track of the total amount of loss 
                     total_loss = total_loss + obj.loss(y{data_idx}, data);
                     
                     cur_d_loss = obj.d_loss(y{data_idx}, data);
 %                     disp(["Loss" cur_d_loss]);
                     
-                    %Runs through the layers backwards
+                    % Gradient Descent
                     for idx = length(obj.layers):-1:1
 %                         disp(["LAYER# " idx])
-                        % Backpropogation occurs based on the layer, the derivative's current loss, and learn rate
+                        % Backpropogation occurs based on the layer, the derivative's current loss, and learning rate
                         [cur_d_loss, obj.layers{idx}]= back(obj.layers{idx}, cur_d_loss, LR);
                     end
     
