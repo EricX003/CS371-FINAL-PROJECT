e0558aed1b2e9bfc8eccf361f7aca3cd3e69f293
ThisIsntPermanent <102081493+ThisIsntPermanent@users.noreply.github.com>
2022-04-28 16:07:14 -0500

    Update Convolutional_Layer.m

--- WeekThree/Convolutional_Layer.m
+++ WeekThree/Convolutional_Layer.m
@@ -8,26 +8,31 @@ classdef Convolutional_Layer
     end
 
     methods(Static = false)
-
+        
+        % Function initializes the kernel according to the depth and size
         function obj = Convolutional_Layer(depth, size)
             obj.kernel_size = size;
             obj.kernel = randn(depth, obj.kernel_size, obj.kernel_size) / 9;
             obj.depth = depth;
         end
-
+        
+        % Function to perform forward propogation 
         function [obj, out] = forward(obj, in)
             obj.in = in;
             [height, width] = size(in);
             out = zeros(height - obj.kernel_size + 1, width - obj.kernel_size + 1, obj.depth);
-
+   
             for x = 1:height - obj.kernel_size + 1
                 for y = 1: width - obj.kernel_size + 1
+                
+                    % Performs a dot product between the input and kernel
                     out(x, y) = sum(in(x : x + obj.kernel_size - 1, y : y + obj.kernel_size - 1) .* obj.kernel, ["all"]);
                 end
             end
 
         end
-
+        
+        % Function to perform backpropagation
         function [obj, dLdF] = back(obj, dLdOut, LR)
 
             dLdF = zeros(size(obj.kernel));
