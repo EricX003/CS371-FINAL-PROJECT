1f82bef2a4f0442e7291e91ca05b93490b1e9538
ThisIsntPermanent <102081493+ThisIsntPermanent@users.noreply.github.com>
2022-04-28 23:35:20 -0500

    Update Test_CNN.m

--- Test_CNN.m
+++ Test_CNN.m
@@ -1,10 +1,14 @@
+% Test file to ensure our CNN runs
+
 x = {zeros(6) + 1, zeros(6) + 1};
 y = {[1, 0]; [0, 1]};
 
 test_net = CNN(2);
 
+% Calls upon the functions to ensure they work
 test_net = test_net.add(Convolutional_Layer(3, 10));
 test_net = test_net.add(MaxPooling(2));
 test_net = test_net.add(SoftMax(2 * 2 * 10, 2));
 
+% Trains the network with the given epochs and initial learning rate 
 test_net = test_net.train(x, y, 100000, 0.001);
